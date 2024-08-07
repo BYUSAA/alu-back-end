57cedbe8cc496204b6342e7f358b6fd255a6f8fd
BYUSAA <byuussaa@gmail.com>
2024-08-07 17:40:14 +0100

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,26 +4,35 @@
 import requests
 import sys
 
-
 def main():
     """main function"""
+    if len(sys.argv) != 2:
+        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
+
     user_id = int(sys.argv[1])
     todo_url = 'https://jsonplaceholder.typicode.com/todos'
     user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
 
     response = requests.get(todo_url)
+    if response.status_code != 200:
+        print("Error: Failed to retrieve todos")
+        sys.exit(1)
 
     total_questions = 0
     completed = []
     for todo in response.json():
-
         if todo['userId'] == user_id:
             total_questions += 1
-
             if todo['completed']:
                 completed.append(todo['title'])
 
-    user_name = requests.get(user_url).json()['name']
+    user_response = requests.get(user_url)
+    if user_response.status_code != 200:
+        print("Error: Failed to retrieve user information")
+        sys.exit(1)
+
+    user_name = user_response.json()['name']
 
     printer = ("Employee {} is done with tasks({}/{}):".format(user_name,
                len(completed), total_questions))

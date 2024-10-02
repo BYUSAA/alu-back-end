828b0d64715f2ceb959830593158a745075472d5
BYUSAA <byuussaa@gmail.com>
2024-10-02 14:32:49 +0100

    updates adds

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,52 +1,41 @@
 #!/usr/bin/python3
-"""
-Python script that returns TODO list progress for a given employee ID
-"""
+"""Script that gets user data (Todo list) from API
+and then export the result to csv file. """
+
+import csv
 import requests
-from sys import argv
+import sys
 
 
-def get_employee_info(employee_id):
-    """
-    Get employee information by employee ID
-    """
-    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/'
-    response = requests.get(url)
-    return response.json()
+def main():
+    """main function"""
+    user_id = int(sys.argv[1])
+    todo_url = 'https://jsonplaceholder.typicode.com/todos'
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
 
+    file_content = []
 
-def get_employee_todos(employee_id):
-    """
-    Get the TODO list of the employee by employee ID
-    """
-    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
-    response = requests.get(url)
-    return response.json()
+    response = requests.get(todo_url)
+    user_name = requests.get(user_url).json().get('username')
 
+    for todo in response.json():
+        if todo.get('userId') == user_id:
+            file_content.append(
+                [str(user_id),
+                 user_name,
+                 todo.get('completed'),
+                 "{}".format(todo.get('title'))])
 
-def main(employee_id):
-    """
-    Main function to fetch and display the TODO list progress of the employee
-    """
-    employee = get_employee_info(employee_id)
-    employee_name = employee.get("name")
-
-    emp_todos = get_employee_todos(employee_id)
-    tasks = {todo.get("title"): todo.get("completed") for todo in emp_todos}
-
-    total_tasks = len(tasks)
-    completed_tasks = [completed for completed in tasks.values() if completed]
-    completed_tasks_count = len(completed_tasks)
-
-    print(f"Employee {employee_name} is done with tasks"
-          f"({completed_tasks_count}/{total_tasks}):")
-    for title, completed in tasks.items():
-        if completed:
-            print(f"\t {title}")
+    print(file_content)
+    file_name = "{}.csv".format(user_id)
+    with open(file_name, 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        for row in file_content:
+            for item in row:
+                str(item)
+            csv_writer.writerow(row)
+        print('file written successfully')
 
 
 if __name__ == "__main__":
-    if len(argv) > 1:
-        main(argv[1])
-    else:
-        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
+    main()
